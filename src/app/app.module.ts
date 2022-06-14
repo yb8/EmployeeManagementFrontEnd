d6ccbd330028058bcf3dd185c712000d2416feaf
youssef <youssef.bejaoui8@gmail.com>
2022-06-14 16:53:12 +0300

    FrontEnd Final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { EmployeeService } from './employee.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee.service.ts
@@ -0,0 +1,36 @@
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployees(employee: Employee): Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployees(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployees(employeeId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+
+
+
+
+
+}
